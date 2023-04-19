3153d7f6e4c93072ca605db510ca7f89f454f6cd
bicco2 <jhb_0921@naver.com>
2023-04-19 23:20:14 +0900

    chore : divide nav page

--- src/views/admin-category/admin-category.js
+++ src/views/admin-category/admin-category.js
@@ -8,37 +8,21 @@ const categoryContent = document.getElementById('category-manage-content');
 const productContent = document.getElementById('product-manage-content');
 
 // 초기 주문관리 선택
-orderLink.classList.toggle('bg-lime-500');
-
-// 버튼 누를 시 다른 요소 색 x
-function resetLinks() {
-  orderLink.classList.remove('bg-lime-500');
-  categoryLink.classList.remove('bg-lime-500');
-  productLink.classList.remove('bg-lime-500');
-}
+categoryLink.classList.toggle('bg-lime-500');
 
 orderLink.addEventListener('click', () => {
-  resetLinks();
-  orderLink.classList.toggle('bg-lime-500');
-
   orderContent.style.display = 'block';
   categoryContent.style.display = 'none';
   productContent.style.display = 'none';
 });
 
 categoryLink.addEventListener('click', () => {
-  resetLinks();
-  categoryLink.classList.toggle('bg-lime-500');
-
   orderContent.style.display = 'none';
   categoryContent.style.display = 'block';
   productContent.style.display = 'none';
 });
 
 productLink.addEventListener('click', () => {
-  resetLinks();
-  productLink.classList.toggle('bg-lime-500');
-
   orderContent.style.display = 'none';
   categoryContent.style.display = 'none';
   productContent.style.display = 'block';
---
+++ src/views/admin-order/admin-order.js
@@ -0,0 +1,29 @@
+// 요소(element), input 혹은 상수
+const orderLink = document.getElementById('order-manage-link');
+const categoryLink = document.getElementById('category-manage-link');
+const productLink = document.getElementById('product-manage-link');
+
+const orderContent = document.getElementById('order-manage-content');
+const categoryContent = document.getElementById('category-manage-content');
+const productContent = document.getElementById('product-manage-content');
+
+// 초기 주문관리 선택
+orderLink.classList.toggle('bg-lime-500');
+
+orderLink.addEventListener('click', () => {
+  orderContent.style.display = 'block';
+  categoryContent.style.display = 'none';
+  productContent.style.display = 'none';
+});
+
+categoryLink.addEventListener('click', () => {
+  orderContent.style.display = 'none';
+  categoryContent.style.display = 'block';
+  productContent.style.display = 'none';
+});
+
+productLink.addEventListener('click', () => {
+  orderContent.style.display = 'none';
+  categoryContent.style.display = 'none';
+  productContent.style.display = 'block';
+});
