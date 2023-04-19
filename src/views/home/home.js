c2598dbdaca52e388659bf86d1f66845e6af8e61
shinhayeong <judy.hayoung.shin@gmail.com>
2023-04-19 16:30:29 +0900

    feat: home ui

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -10,31 +10,32 @@ const productCounter = document.querySelector('#product-counter');
 const card = document.querySelector('#card');
 
 const createCard = () => {
-  return ` <div id="card">
-  <a class="card-link" href="#">
-    <img
-      src="../dist/example.jpeg"
-      id="card-img-top"
-      class="rounded-lg"
-      width="350" height="376"
-    />
-    <div id="card-body">
-      <div id="card-text card-title" class="text-lg">
-        하얗게 피어난 얼음꽃
+  return ` 
+  <div id="card" style="width:350px; height:480px;">
+    <a id="card-link" href="#">
+      <img
+        src="../dist/example.jpeg"
+        id="card-img-top"
+        class="rounded-lg"
+        width="350" height="376"
+      />
+      <div id="card-body">
+        <div id="card-text card-title" class="text-lg mt-3 mb-1">
+          하얗게 피어난 얼음꽃
+        </div>
+        <div id="card-text card-hashtags" class="text-gray-500 mt-1 mb-1">
+          #관엽/공기정화식물
+        </div>
+        <div id="card-text card-price" class="font-bold text-base mt-1 mb-1">
+          14,000 원
+        </div>
       </div>
-      <div id="card-text card-hashtags" class="text-gray-500">
-        #관엽/공기정화식물
-      </div>
-      <div id="card-text card-price" class="font-bold text-base">
-        14,000 원
-      </div>
-    </div>
-  </a>
-</div>`;
+    </a>
+  </div>`;
 };
 
 // 임시로 화면에 뿌리는 것임 UI상 어케 보이나
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i < 9; i++) {
   cards.innerHTML += createCard();
 }
 
