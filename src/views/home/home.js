f9803fc82dacddc0e1493292d84f224063f0d334
shinhayeong <judy.hayoung.shin@gmail.com>
2023-04-19 15:23:16 +0900

    feat: card list ui

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,3 +1,59 @@
+// const createCategory = (category) => {
+//   return `
+//         <li class="nav-item category">
+//           <div class="nav-link">#${category.name}</div>
+//         </li>`;
+// };
+
+const cards = document.querySelector('#item-cards-list');
+const productCounter = document.querySelector('#product-counter');
+const card = document.querySelector('#card');
+
+const createCard = () => {
+  return ` <div id="card">
+  <a class="card-link" href="#">
+    <img
+      src="../dist/example.jpeg"
+      id="card-img-top"
+      class="rounded-lg"
+      width="350" height="376"
+    />
+    <div id="card-body">
+      <div id="card-text card-title" class="text-lg">
+        하얗게 피어난 얼음꽃
+      </div>
+      <div id="card-text card-hashtags" class="text-gray-500">
+        #관엽/공기정화식물
+      </div>
+      <div id="card-text card-price" class="font-bold text-base">
+        14,000 원
+      </div>
+    </div>
+  </a>
+</div>`;
+};
+
+// 임시로 화면에 뿌리는 것임 UI상 어케 보이나
+for (let i = 0; i <= 10; i++) {
+  cards.innerHTML += createCard();
+}
+
+// const createCard = (item) => {
+//   return `<div id="item-cards-list">
+//     <a class="card-link" href='/products/${item._id}'>
+//       <img src="${item.smallImageURL}" class="card-img-top" alt="${
+//     item.name
+//   }" />
+//       <div class="card-body">
+//       <div class="card-text card-title">${item.name}</div>
+//       <div class="card-text card-hashtags">${item.category}</div>
+//       <div class="card-text card-price">₩ ${addCommas(item.price)}</div>
+//       </div>
+//     </a>
+//     </div>
+//   </div>`;
+// };
+
 // // 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
 // // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // // 코드 예시를 남겨 두었습니다.
