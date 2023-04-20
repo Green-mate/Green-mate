bbf1b5fc786ccef57c2d70d483db4c0eead2d93c
bicco2 <jhb_0921@naver.com>
2023-04-20 17:04:17 +0900

    feat : add admin categorypage

--- src/views/admin-category/admin-category.js
+++ src/views/admin-category/admin-category.js
@@ -1,29 +1,6 @@
-// 요소(element), input 혹은 상수
-const orderLink = document.getElementById('order-manage-link');
-const categoryLink = document.getElementById('category-manage-link');
-const productLink = document.getElementById('product-manage-link');
+const addTest = document.getElementById('addTest');
+const btnTest = document.getElementById('btnTest');
 
-const orderContent = document.getElementById('order-manage-content');
-const categoryContent = document.getElementById('category-manage-content');
-const productContent = document.getElementById('product-manage-content');
-
-// 초기 주문관리 선택
-categoryLink.classList.toggle('bg-lime-500');
-
-orderLink.addEventListener('click', () => {
-  orderContent.style.display = 'block';
-  categoryContent.style.display = 'none';
-  productContent.style.display = 'none';
-});
-
-categoryLink.addEventListener('click', () => {
-  orderContent.style.display = 'none';
-  categoryContent.style.display = 'block';
-  productContent.style.display = 'none';
-});
-
-productLink.addEventListener('click', () => {
-  orderContent.style.display = 'none';
-  categoryContent.style.display = 'none';
-  productContent.style.display = 'block';
+btnTest.addEventListener('click', () => {
+  addTest.style.display = 'block';
 });
