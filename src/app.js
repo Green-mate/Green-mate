42fe67e06bc08436333fbf33eafc52904e24f483
Hanna <foureaf@gmail.com>
2023-04-21 22:47:05 +0900

    fix : product부분 임시 주석처리

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import cors from "cors";
-import morgan from "morgan";
-import express from "express";
+import cors from 'cors';
+import morgan from 'morgan';
+import express from 'express';
 import {
   viewsRouter,
   userRouter,
@@ -14,13 +14,13 @@ const app = express();
 
 // 요청과 응답에 대한 정보를 콘솔에 기록하는 morgan 미들웨어 추가.(app.js 가장 위에 있어야 함!)
 // 나중에 배포환경, 개발환경에 따라 케이스를 나눠야 하지만, 일단은 개발환경만 연결.
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 // 더 자세한 로그를 보고싶다면 위 대신 아래를 사용한다.
 // app.use(morgan('combined'));
 
 // CORS 에러 방지
 app.use(cors());
-app.use(express.static('src'));
+
 // Content-Type: application/json 형태의 데이터를 인식하고 핸들링할 수 있게 함.
 app.use(express.json());
 
@@ -34,11 +34,11 @@ app.use(viewsRouter);
 // 아래처럼 하면, userRouter 에서 '/login' 으로 만든 것이 실제로는 앞에 /api가 붙어서
 // /api/login 으로 요청을 해야 하게 됨. 백엔드용 라우팅을 구분하기 위함임.
 
-app.use("/api", userRouter);
+app.use('/api', userRouter);
 
-app.use("/api", productRouter);
+// app.use("/api", productRouter);
 
-app.use("/api", categoryRouter);
+app.use('/api', categoryRouter);
 
 app.use('/api', orderRouter);
 
