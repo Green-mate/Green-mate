5ab2002b98712a5e8b6bb021f2fcd4b184f61e43
[SW4/1반] 김희삼 <heesamk9245@gmail.com>
2023-04-22 18:15:14 +0900

    test: indexedDB, cart

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,3 +1,7 @@
+import * as API from '../api.js';
+
+////////////////
+
 const goodsDetail = document.querySelector('#goods-detail-section');
 
 function createCard() {
@@ -24,6 +28,8 @@ function createCard() {
 for (let i = 0; i < 5; i++) {
   createCard();
 }
+
+//////////////////
 
 const postalCodeInput = document.querySelector('#postal-code');
 const searchAddressButton = document.querySelector('#search-address-button');
@@ -68,6 +74,8 @@ function searchAddress(e) {
 
 searchAddressButton.addEventListener('click', searchAddress);
 
+///////////////
+
 const requestSelectBox = document.querySelector('#request-select-box');
 const customRequestContainer = document.querySelector(
   '#custom-request-container',
@@ -94,3 +102,100 @@ function handleRequestChange(e) {
 }
 
 requestSelectBox.addEventListener('change', handleRequestChange);
+
+///////////////////
+
+const nameInput = document.querySelector('#name-input');
+const phoneNumberInput = document.querySelector('#phone-input');
+const totalPriceContainer = document.querySelector('#total-price');
+
+const doOrderButton = document.querySelector('#order-button');
+doOrderButton.addEventListener('click', order);
+
+async function order() {
+  const receiverName = nameInput.value;
+  const receiverPhoneNumber = phoneNumberInput.value;
+  const postalCode = postalCodeInput.value;
+  const address1 = addressMainInput.value;
+  const address2 = addressSubInput.value;
+  const requestType = requestSelectBox.value;
+  const customRequest = customRequestInput.value;
+  const totalPrice = convertToNumber(totalPriceContainer.innerText);
+  const { selectedIds } = await getFromDb('order', 'summary');
+
+  if (!receiverName || !receiverPhoneNumber || !postalCode || !address2) {
+    return alert('배송지 정보를 모두 입력해 주세요.');
+  }
+
+  // 요청사항의 종류에 따라 request 문구가 달라짐
+  let request;
+
+  if (requestType === '0') {
+    request = '요청사항 없음.';
+  } else if (requestType === '6') {
+    if (!customRequest) {
+      return alert('요청사항을 작성해 주세요.');
+    }
+    request = customRequest;
+  } else {
+    request = requestOption[requestType];
+  }
+
+  const address = {
+    postalCode,
+    address1,
+    address2,
+    receiverName,
+    receiverPhoneNumber,
+  };
+
+  try {
+    // 전체 주문을 등록함
+    const orderData = await API.post('/api/order', {
+      totalPrice,
+      address,
+      request,
+    });
+
+    const orderId = orderData._id;
+
+    // 제품별로 주문아이템을 등록함
+    for (const productId of selectedIds) {
+      const { quantity, price } = await getFromDb('cart', productId);
+      const totalPrice = quantity * price;
+
+      await API.post('/api/orderitem', {
+        orderId,
+        productId,
+        quantity,
+        totalPrice,
+      });
+
+      // indexedDB에서 해당 제품 관련 데이터를 제거함
+      await deleteFromDb('cart', productId);
+      await putToDb('order', 'summary', (data) => {
+        data.ids = data.ids.filter((id) => id !== productId);
+        data.selectedIds = data.selectedIds.filter((id) => id !== productId);
+        data.productsCount -= 1;
+        data.productsTotal -= totalPrice;
+      });
+    }
+
+    // 입력된 배송지정보를 유저db에 등록함
+    const data = {
+      phoneNumber: receiverPhoneNumber,
+      address: {
+        postalCode,
+        address1,
+        address2,
+      },
+    };
+    await API.post('/api/user/deliveryinfo', data);
+
+    alert('결제 및 주문이 정상적으로 완료되었습니다.\n감사합니다.');
+    window.location.href = '/order/complete';
+  } catch (err) {
+    console.log(err);
+    alert(`결제 중 문제가 발생하였습니다: ${err.message}`);
+  }
+}
