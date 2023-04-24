fe7261d5fdcfe83c5853e3ae58f9e513f1ff2bc6
Hanna <foureaf@gmail.com>
2023-04-24 13:40:34 +0900

    fix: fix conflict

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-export * from "./category-service";
-export * from "./user-service";
-export * from "./product-service";
-=======
 export * from './category-service';
 export * from './user-service';
+export * from './product-service';
 export * from './order-service';
->>>>>>> d6046cb0682f3abe94adb0617ea9bc4f2b86f354
--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-import { productModel } from "../db";
+import { productModel } from '../db';
 
 export class ProductService {
   constructor(productModel) {
@@ -9,7 +8,7 @@ export class ProductService {
   async getProductsCountAll() {
     const count = await this.productModel.countDocumentsAll();
     if (count === null) {
-      throw new Error("상품 개수를 불러올 수 없습니다.");
+      throw new Error('상품 개수를 불러올 수 없습니다.');
     }
     console.log(`count: ${count}`);
     return count;
@@ -19,7 +18,7 @@ export class ProductService {
   async getProductsCount(category) {
     const count = await this.productModel.countDocumentsByCategory(category);
     if (count === null) {
-      throw new Error("상품 개수를 불러올 수 없습니다.");
+      throw new Error('상품 개수를 불러올 수 없습니다.');
     }
     console.log(`count: ${count}`);
     return count;
@@ -30,11 +29,11 @@ export class ProductService {
     const products = await this.productModel.findAll();
 
     if (products === null) {
-      throw new Error("상품 목록을 불러올 수 없습니다.");
+      throw new Error('상품 목록을 불러올 수 없습니다.');
     }
     console.log(
-      "🚀 ~ file: product-service.js:38 ~ ProductService ~ getProducts ~ products:",
-      products
+      '🚀 ~ file: product-service.js:38 ~ ProductService ~ getProducts ~ products:',
+      products,
     );
     return products;
   }
@@ -43,12 +42,12 @@ export class ProductService {
   async getProducts(category) {
     const products = await this.productModel.findAllByCategory(category);
     console.log(
-      "🚀 ~ file: product-service.js:36 ~ ProductService ~ getProducts ~ products:",
-      products
+      '🚀 ~ file: product-service.js:36 ~ ProductService ~ getProducts ~ products:',
+      products,
     );
 
     if (products === null) {
-      throw new Error("상품 목록을 불러올 수 없습니다.");
+      throw new Error('상품 목록을 불러올 수 없습니다.');
     }
 
     return products;
@@ -59,7 +58,7 @@ export class ProductService {
     const products = await this.productModel.findByShortId(shortId);
 
     if (products === null) {
-      throw new Error("해당하는 상품이 존재하지 않습니다.");
+      throw new Error('해당하는 상품이 존재하지 않습니다.');
     }
 
     return products;
@@ -76,15 +75,15 @@ export class ProductService {
   async addProducts(productObj) {
     const { productName } = productObj;
     const productNameDB = await this.productModel.findByProductName(
-      productName
+      productName,
     );
     if (productNameDB) {
-      throw new Error("이미 존재하는 상품입니다.");
+      throw new Error('이미 존재하는 상품입니다.');
     }
 
     const newProduct = await this.productModel.create(productObj);
     console.log(newProduct);
-    return "success";
+    return 'success';
   }
 
   // 상품 업데이트
@@ -92,12 +91,12 @@ export class ProductService {
     const updateProduct = await this.productModel.update(item, updateObj);
 
     if (updateProduct === null) {
-      throw new Error("해당 상품을 찾을 수 없어 수정할 수 없습니다.");
+      throw new Error('해당 상품을 찾을 수 없어 수정할 수 없습니다.');
     }
 
     console.log(`수정된 데이터: ${updateProduct}`);
 
-    return "success";
+    return 'success';
   }
 
   // 상품 삭제
@@ -105,62 +104,13 @@ export class ProductService {
     const deleteProduct = await this.productModel.delete(item);
 
     if (deleteProduct === null) {
-      throw new Error("해당 상품을 찾을 수 없어 삭제에 실패했습니다.");
+      throw new Error('해당 상품을 찾을 수 없어 삭제에 실패했습니다.');
     }
 
-    return "success";
+    return 'success';
   }
 }
 
 const productService = new ProductService(productModel);
-=======
-// import { categoryModel, Product } from "../db"; // Product는 Product model임.
-// /*
-// status(200) - 성공
-// 해당되는 상품 목록을 객체리스트로 받아옵니다.
-// products = [
-//   {
-//     product_name,
-//     product_price,
-//     productImage,
-//     stock,
-//   },
-//   {…},
-// ]
-// 전체 또는 카테고리에 해당하는 상품 개수를 받아옵니다.
-// countProducts = 109 // 전체 상품 수: 109개
 
-// status(400) - 실패(DB에서 카테고리를 찾을 수 없는 경우)
-// {
-// "result": "error",
-// "reason": “카테고리가 존재하지 않습니다."
-// }
-
-// status(400) - 실패(페이지를 찾을 수 없는 경우)
-// {
-// "result": "error",
-// "reason": “해당 페이지가 존재하지 않습니다."
-// }
-// */
-// class ProductService {
-//   constructor(categoryModel, Product) {
-//     this.categoryModel = categoryModel;
-//     this.productModel = Product;
-//   }
-//   // 주어진 데이터가 데이터베이스에 하나 이상 존재하는 경우에는 그 값을 반환하고, 아니면 null
-//   async getProduct(category) {
-//     const products = await this.Product.exists({ category });
-
-//     // db에서 찾지 못한 경우, 에러 메시지 반환
-//     if (products === null) {
-//       throw new Error("카테고리가 존재하지 않습니다.");
-//     } else {
-//       return products;
-//     }
-//   }
-// }
-
-// const productService = new ProductService(categoryModel, Product);
->>>>>>> d6046cb0682f3abe94adb0617ea9bc4f2b86f354
-
-// export { productService };
+export { productService };
