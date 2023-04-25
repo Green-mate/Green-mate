2e93811460f10879f712d8afb7180ee2576e35b6
shinhayeong <judy.hayoung.shin@gmail.com>
2023-04-26 04:58:45 +0900

    feat : 상품 상세 장바구니, 주문 indexedDB 연결

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -1,9 +1,6 @@
 import * as API from '../api.js';
 import { getUrlParams } from '/useful-functions.js';
-
-// const image = document.getElementById('card-img-top');
-// const productPriceText = document.getElementById('product-price');
-// const categorySmall = document.getElementById('category-name');
+import { addToDb, putToDb } from '../indexed-DB.js';
 
 let productAmount = document.getElementById('product-amount');
 let productAmountNum = parseInt(productAmount.textContent);
@@ -11,24 +8,19 @@ let productAmountNum = parseInt(productAmount.textContent);
 const totalAmount = document.getElementById('total-number');
 const totalPrice = document.getElementById('total-price');
 
-const purchaseBtn = document.getElementById('purchase-btn');
-const cartBtn = document.getElementById('cart-btn');
-
-// const categoryInfo = document.getElementById('product-category-info');
-// const productAmountInfo = document.getElementById('product-amount-info');
-
 let TOTALPRICE = 0;
 productAmountNum = 1;
 
 // 상품 아이디 정보
-const param = getUrlParams();
-const pid = param.pid;
+const { pid } = getUrlParams();
 
 getProductDetail();
 async function getProductDetail() {
   const response = await API.getWithoutToken(`/api/products`, pid);
+  console.log(response);
   const { category, productImage, productName, productPrice, stock } = response;
 
+  /* 값 렌더링 */
   const name = document.getElementById('product-name');
   const image = document.getElementById('card-img-top');
   const productPriceText = document.getElementById('product-price');
@@ -67,4 +59,55 @@ async function getProductDetail() {
   TOTALPRICE += productAmountNum * productPrice;
   totalAmount.innerText = productAmountNum;
   totalPrice.innerText = TOTALPRICE;
+
+  /* 장바구니 및 바로 구매 기능 */
+  const orderBtn = document.getElementById('order-button');
+  const cartBtn = document.getElementById('cart-button');
+
+  cartBtn.addEventListener('click', async () => {
+    try {
+      await insertCart(response);
+      alert('장바구니에 추가되었습니다!🧺');
+    } catch (err) {
+      if (err.message.includes('Key')) {
+        alert('이미 장바구니에 추가되어 있습니다.');
+      }
+      console.log(err);
+    }
+  });
+
+  orderBtn.addEventListener('click', async () => {
+    try {
+      await insertCart(response);
+      window.location.replace('/order');
+    } catch (err) {
+      if (err.message.includes('Key')) {
+        alert('이미 장바구니에 추가되어 있습니다.');
+        window.location.replace('/order');
+      }
+      console.log(err);
+    }
+  });
+}
+
+async function insertCart(product) {
+  const { _id: id, shortId, productPrice } = product;
+
+  await addToDb('cart', { ...product, quantity: 1 }, id);
+
+  await putToDb('order', 'total-order', (data) => {
+    const totalCount = data.productsCount;
+    const totalPrice = data.productsTotalPrice;
+    const ids = data.ids;
+    const shortIds = data.shortIds;
+    const selectedIds = data.selectedIds;
+
+    data.productsCount = totalCount ? totalCount + 1 : 1;
+    data.productsTotalPrice = totalPrice
+      ? totalPrice + productPrice
+      : productPrice;
+    data.ids = ids ? [...ids, id] : [id];
+    data.shortIds = shortIds ? [...shortIds, shortId] : [shortId];
+    data.selectedIds = selectedIds ? [...selectedIds, id] : [id];
+  });
 }
