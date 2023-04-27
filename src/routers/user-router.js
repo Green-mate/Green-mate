1dcd94e4db02c1fde84618bb11cfb5cf9c2def97
마리나 김 <akflsk6040@gmail.com>
2023-04-27 11:53:17 +0900

    fix: (BE) 주문, 유저스키마 날짜 수정

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -1,37 +1,40 @@
-import { Router } from 'express';
-import is from '@sindresorhus/is';
-import { errorHandler, loginRequired, asyncHandler } from '../middlewares';
-import { userService } from '../services';
+import { Router } from "express";
+import is from "@sindresorhus/is";
+import { errorHandler, loginRequired, asyncHandler } from "../middlewares";
+import { userService } from "../services";
+import { krDate } from "../utils";
 
 const userRouter = Router();
 
 userRouter.post(
-  '/users/join',
+  "/users/join",
   asyncHandler(async (req, res, next) => {
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요',
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
     const { userName, email, password, role } = req.body;
+    const date = krDate();
     const newUser = await userService.addUser({
       userName,
       email,
       password,
       role,
+      createdDate: date,
     });
     // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
     res.status(201).json(newUser);
-  }),
+  })
 );
 
 // 로그인 api (아래는 /login 이지만, 실제로는 /api/users/login로 요청해야 함.)
 userRouter.post(
-  '/users/login',
+  "/users/login",
   asyncHandler(async function (req, res) {
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요',
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
 
@@ -43,29 +46,29 @@ userRouter.post(
     });
     // jwt 토큰을 프론트에 보냄 (jwt 토큰은, 문자열임)
     res.status(200).json(userInfoWithUserToken);
-  }),
+  })
 );
 
 // uid에 해당하는 유저 개인의 목록을 가져옴 (단일)
 userRouter.get(
-  '/users/:uid',
+  "/users/:uid",
   loginRequired,
   asyncHandler(async function (req, res, next) {
     const userId = req.params.uid;
     const user = await userService.getUser(userId);
 
     res.status(200).json(user);
-  }),
+  })
 );
 
 // 사용자 정보 수정
 userRouter.patch(
-  '/users/:uid',
+  "/users/:uid",
   loginRequired,
   asyncHandler(async function (req, res, next) {
     if (is.emptyObject(req.body)) {
       throw new Error(
-        'headers의 Content-Type을 application/json으로 설정해주세요',
+        "headers의 Content-Type을 application/json으로 설정해주세요"
       );
     }
 
@@ -85,22 +88,22 @@ userRouter.patch(
     // 사용자 정보를 업데이트함.
     const updatedUserInfo = await userService.setUser(
       userInfoRequired,
-      toUpdate,
+      toUpdate
     );
 
     res.status(200).json(updatedUserInfo);
-  }),
+  })
 );
 
 //유저 role disabled로 변경 (회원탈퇴)
 userRouter.patch(
-  '/users/delete/:uid',
+  "/users/delete/:uid",
   loginRequired,
   asyncHandler(async (req, res, next) => {
     const userId = req.params.uid;
     const deletedUserInfo = await userService.deleteUser(userId);
     res.status(200).json(deletedUserInfo);
-  }),
+  })
 );
 
 export { userRouter };
