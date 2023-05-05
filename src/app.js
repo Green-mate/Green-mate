936d32320b02c46731a9dfa02bb0e6ecb3064b0d
ryuiseo <rks85227@gmail.com>
2023-05-05 10:55:04 +0900

    fix : multer

--- src/app.js
+++ src/app.js
@@ -46,6 +46,7 @@ if (process.env.NODE_ENV !== 'production') {
 app.use(viewsRouter);
 
 // api 라우팅
+app.use('/static', express.static('public')); //정적파일 관리 경로
 app.use('/api', userRouter);
 app.use('/api', productRouter);
 app.use('/api', categoryRouter);
--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -5,7 +5,7 @@ import { productService } from '../services';
 import { pagenate, krDate } from '../utils';
 import { logger } from '../../config/winston';
 import { productImageUpload } from '../middlewares/multer';
-
+const fs = require('fs');
 const productRouter = Router();
 
 // 상품 전체 조회: /api/products?page=1&perPage=9
@@ -75,22 +75,22 @@ productRouter.get(
 productRouter.post(
   '/admin/products',
   adminOnly,
-  productImageUpload.single('productImage'),
+  productImageUpload,
   asyncHandler(async (req, res, next) => {
     if (is.emptyObject(req.body)) {
       throw new Error(
         'headers의 Content-Type이 application/json으로 설정되지 않았습니다.',
       );
     }
-    const { productName, category, productPrice, productImage, stock } =
-      req.body;
+    const imgName = `http://localhost:3000/static/${req.file.filename}`;
+    const { productName, category, productPrice, stock } = req.body;
     const date = krDate(); // 한국 시간대로 설정.
 
     const productObj = {
       productName,
       category,
       productPrice: Number(productPrice),
-      productImage: '../dist/product-images/' + req.file.filename,
+      productImage: imgName,
       stock: Number(stock),
       createdDate: date,
     };
