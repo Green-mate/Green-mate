10127b28dcf65ba37265c4d79c9bd4716cbac74f
ryuiseo <rks85227@gmail.com>
2023-05-06 14:08:41 +0900

    fix : product patch multer

--- src/routers/product-router.js
+++ src/routers/product-router.js
@@ -82,7 +82,7 @@ productRouter.post(
         'headers의 Content-Type이 application/json으로 설정되지 않았습니다.',
       );
     }
-    const imgName = `http://kdt-sw-4-team08.elicecoding.com/static/${req.file.filename}`;
+    const imgName = `http://localhost:3000/static/${req.file.filename}`;
     //도메인 바뀌면 바꿔주기
     const { productName, category, productPrice, stock } = req.body;
     const date = krDate(); // 한국 시간대로 설정.
@@ -110,13 +110,14 @@ productRouter.post(
 productRouter.patch(
   '/admin/products',
   adminOnly,
+  productImageUpload,
   asyncHandler(async (req, res, next) => {
     if (is.emptyObject(req.body)) {
       throw new Error(
         'headers의 Content-Type이 application/json으로 설정되지 않았습니다.',
       );
     }
-
+    const imgName = `http://localhost:3000/static/${req.file.filename}`;
     const item = req.query.item;
     logger.info(item);
     const { productName, category, productPrice, productImage, stock } =
@@ -126,7 +127,7 @@ productRouter.patch(
       productName,
       category,
       productPrice: Number(productPrice),
-      productImage,
+      productImage: imgName,
       stock: Number(stock),
     };
     const products = await productService.updateProduct(item, updateObj);
